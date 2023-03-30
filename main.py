d9c778a742cff90b0efa3136129f0e25c6ecdf43
Joraarce <65260605+Joraarce@users.noreply.github.com>
2023-03-30 11:37:12 -0600

    Update main.py
    Se agregaron las funciones de resta, división, multiplicación y simplificar

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from sympy.parsing.latex import parse_latex
 # r"\frac {1 + \sqrt {\a}} {\b} = x^2"
 # Esto,para que python pueda leer correctamente los backlashes
 
-def suma(ecuacion, factor):
+def Sumar(ecuacion, factor):
     parts = ecuacion.split("=")  # Divide la ecuacion en dos partes, dividiendo en el igual
     fac = parse_latex(factor) # convierte de latex a formato Sympy
     eqLeft = parse_latex(parts[0]) + fac # Se suma al lado izquierdo el factor
@@ -23,8 +23,46 @@ def suma(ecuacion, factor):
     eqLatex = latex(Eq(eqLeft, eqRight)) # Se combinan los dos lados en una nueva ecuación, en formato latex
     return eqLatex  #Se devuelve la ecuacion en latex
 
+def Restar(ecuacion, factor):
+    parts = ecuacion.split("=")  # Divide la ecuacion en dos partes, dividiendo en el igual
+    fac = parse_latex(factor) # convierte de latex a formato Sympy
+    eqLeft = parse_latex(parts[0]) - fac # Se suma al lado izquierdo el factor
+    eqRight = parse_latex(parts[1]) - fac # Se suma al lado derecho el factor
+    eqLatex = latex(Eq(eqLeft, eqRight)) # Se combinan los dos lados en una nueva ecuación, en formato latex
+    return eqLatex  #Se devuelve la ecuacion en latex
+
+def Multiplicar(ecuacion, factor):
+    parts = ecuacion.split("=")  # Divide la ecuacion en dos partes, dividiendo en el igual
+    fac = parse_latex(factor) # convierte de latex a formato Sympy
+    eqLeft = parse_latex(parts[0]) * fac # Se suma al lado izquierdo el factor
+    eqRight = parse_latex(parts[1]) * fac # Se suma al lado derecho el factor
+    eqLatex = latex(Eq(eqLeft, eqRight)) # Se combinan los dos lados en una nueva ecuación, en formato latex
+    return eqLatex  #Se devuelve la ecuacion en latex
+
+def Dividir(ecuacion, factor):
+    parts = ecuacion.split("=")  # Divide la ecuacion en dos partes, dividiendo en el igual
+    fac = parse_latex(factor) # convierte de latex a formato Sympy
+    eqLeft = parse_latex(parts[0]) / fac # Se suma al lado izquierdo el factor
+    eqRight = parse_latex(parts[1]) / fac # Se suma al lado derecho el factor
+    eqLatex = latex(Eq(eqLeft, eqRight)) # Se combinan los dos lados en una nueva ecuación, en formato latex
+    return eqLatex  #Se devuelve la ecuacion en latex
+
+def Simplificar(ecuacion):
+    parts = ecuacion.split("=")  # Divide la ecuacion en dos partes, dividiendo en el igual
+    eqLeft = powdenest(simplify(parse_latex(parts[0]), inverse=True), force=True)  # Se suma al lado izquierdo el factor
+    eqRight = powdenest(simplify(parse_latex(parts[1]), inverse=True), force=True)  # Se suma al lado derecho el factor
+    eqLatex = latex(Eq(eqLeft, eqRight)) # Se combinan los dos lados en una nueva ecuación, en formato latex
+    return eqLatex  #Se devuelve la ecuacion en latex
+
+
+
 
 ## Pruebas
-print(suma(r"\frac {1 + \sqrt {\a}} {\b} = x^2", "1"))
-print(suma(r"\frac {1 + \sqrt {\a}} {\b} = x^2", "x"))
-print(suma(r"\frac {1 + \sqrt {\a}} {\b} = x^2", "x^2"))
+print(Sumar(r"\frac {1 + \sqrt {\a}} {\b} = x^2", r"1"))
+print(Restar(r"\frac {1 + \sqrt {\a}} {\b} = x^2", r"x"))
+print(Multiplicar(r"\frac {1 + \sqrt {\a}} {\b} = x^2", r"x^2"))
+print(Dividir(r"\frac {1 + \sqrt {\a}} {\b} = x^2", r"x^2"))
+
+print(Simplificar(r"\frac {1 + \sqrt {\a}} {\b} = x^2"))
+print(Simplificar(r"x^2+x+x+1=4"))
+print(Simplificar(r"\frac {1 + \sqrt {\a}} {\b} = x^2"))
