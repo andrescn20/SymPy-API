8b05a33d5c491b1dea3e1bc42bdecf9f93dcaaeb
Andres Castro <andrescn20@gmail.com>
2023-04-08 17:18:30 -0600

    Add missing string conversions

--- functions.py
+++ functions.py
@@ -8,75 +8,76 @@ from sympy import *
 from sympy.parsing.latex import parse_latex
 
 # Aqui comienzo a definir la función de suma
-# Esta funcion recibe como parámetro la ecuacion y el factor en latex
-# Y da como resultado un string con el latex de la nueva ecuacion
+# Esta funcion recibe como parámetro la equation y el factor en latex
+# Y da como resultado un string con el latex de la nueva equation
 
 #Muy importante, los inputs deben venir con una r al inicio, como en
 # r"\frac {1 + \sqrt {\a}} {\b} = x^2"
 # Esto,para que python pueda leer correctamente los backlashes
 
-def Sumar(ecuacion, factor):
+def Sumar(equation, factor):
     fac = parse_expr(factor) # convierte de latex a formato Sympy
-    eqLeft = parse_expr(ecuacion).lhs + fac # Se suma al lado izquierdo el factor
-    eqRight = parse_expr(ecuacion).rhs + fac # Se suma al lado derecho el factor
+    eqLeft = parse_expr(equation).lhs + fac # Se suma al lado izquierdo el factor
+    eqRight = parse_expr(equation).rhs + fac # Se suma al lado derecho el factor
     eq = str(Eq(eqLeft, eqRight)) # Se combinan los dos lados en una nueva ecuación, en formato latex
     eqLatex = latex(Eq(eqLeft, eqRight)) # Se combinan los dos lados en una nueva ecuación, en formato latex
-    return {'sympy' : eq , 'latex' : eqLatex }  #Se devuelve la ecuacion en ambos formatos
+    return {'sympy' : eq , 'latex' : eqLatex }  #Se devuelve la equation en ambos formatos
 
-def Restar(ecuacion, factor):
+def Restar(equation, factor):
     fac = parse_expr(factor) # convierte de latex a formato Sympy
-    eqLeft = parse_expr(ecuacion).lhs - fac # Se suma al lado izquierdo el factor
-    eqRight = parse_expr(ecuacion).rhs - fac # Se suma al lado derecho el factor
-    eq = Eq(eqLeft, eqRight) # Se combinan los dos lados en una nueva ecuación, en formato latex
+    eqLeft = parse_expr(equation).lhs - fac # Se suma al lado izquierdo el factor
+    eqRight = parse_expr(equation).rhs - fac # Se suma al lado derecho el factor
+    eq = str(Eq(eqLeft, eqRight)) # Se combinan los dos lados en una nueva ecuación, en formato latex
     eqLatex = latex(Eq(eqLeft, eqRight)) # Se combinan los dos lados en una nueva ecuación, en formato latex
-    return {'sympy' : eq , 'latex' : eqLatex }  #Se devuelve la ecuacion en ambos formatosx
+    return {'sympy' : eq , 'latex' : eqLatex }  #Se devuelve la equation en ambos formatosx
+ 
 
-def Multiplicar(ecuacion, factor):
+def Multiplicar(equation, factor):
     fac = parse_expr(factor) # convierte de latex a formato Sympy
-    eqLeft = parse_expr(ecuacion).lhs * fac # Se suma al lado izquierdo el factor
-    eqRight = parse_expr(ecuacion).rhs * fac # Se suma al lado derecho el factor
-    eq = Eq(eqLeft, eqRight) # Se combinan los dos lados en una nueva ecuación, en formato latex
+    eqLeft = parse_expr(equation).lhs * fac # Se suma al lado izquierdo el factor
+    eqRight = parse_expr(equation).rhs * fac # Se suma al lado derecho el factor
+    eq = str(Eq(eqLeft, eqRight)) # Se combinan los dos lados en una nueva ecuación, en formato latex
     eqLatex = latex(Eq(eqLeft, eqRight)) # Se combinan los dos lados en una nueva ecuación, en formato latex
-    return {'sympy' : eq , 'latex' : eqLatex }  #Se devuelve la ecuacion en ambos formatos
+    return {'sympy' : eq , 'latex' : eqLatex }  #Se devuelve la equation en ambos formatos
 
-def Dividir(ecuacion, factor):
+def Dividir(equation, factor):
     fac = parse_expr(factor) # convierte de latex a formato Sympy
-    eqLeft = parse_expr(ecuacion).lhs * fac # Se suma al lado izquierdo el factor
-    eqRight = parse_expr(ecuacion).rhs * fac # Se suma al lado derecho el factor
-    eq = Eq(eqLeft, eqRight) # Se combinan los dos lados en una nueva ecuación, en formato latex
+    eqLeft = parse_expr(equation).lhs * fac # Se suma al lado izquierdo el factor
+    eqRight = parse_expr(equation).rhs * fac # Se suma al lado derecho el factor
+    eq = str(Eq(eqLeft, eqRight)) # Se combinan los dos lados en una nueva ecuación, en formato latex
     eqLatex = latex(Eq(eqLeft, eqRight)) # Se combinan los dos lados en una nueva ecuación, en formato latex
-    return {'sympy' : eq , 'latex' : eqLatex }  #Se devuelve la ecuacion en ambos formatos
+    return {'sympy' : eq , 'latex' : eqLatex }  #Se devuelve la equation en ambos formatos
 
-def Potencia(ecuacion, factor):
+def Potencia(equation, factor):
     fac = parse_expr(factor) # convierte de latex a formato Sympy
-    eqLeft = powdenest(Pow(parse_expr(ecuacion).lhs, fac ),
+    eqLeft = powdenest(Pow(parse_expr(equation).lhs, fac ),
                                       force=True) # Se suma al lado izquierdo el factor
-    eqRight = powdenest(Pow(parse_expr(ecuacion).rhs, fac ),
+    eqRight = powdenest(Pow(parse_expr(equation).rhs, fac ),
                                       force=True)# Se suma al lado derecho el factor
-    eq = Eq(eqLeft, eqRight) # Se combinan los dos lados en una nueva ecuación, en formato latex
+    eq = str(Eq(eqLeft, eqRight)) # Se combinan los dos lados en una nueva ecuación, en formato latex
     eqLatex = latex(Eq(eqLeft, eqRight)) # Se combinan los dos lados en una nueva ecuación, en formato latex
-    return {'sympy' : eq , 'latex' : eqLatex }  #Se devuelve la ecuacion en ambos formatos
+    return {'sympy' : eq , 'latex' : eqLatex }  #Se devuelve la equation en ambos formatos
 
-def Raiz(ecuacion, factor):
+def Raiz(equation, factor):
     fac = parse_expr(factor) # convierte de latex a formato Sympy
-    eqLeft = powdenest(root(parse_expr(ecuacion).lhs, fac ),
+    eqLeft = powdenest(root(parse_expr(equation).lhs, fac ),
                                       force=True) # Se suma al lado izquierdo el factor
-    eqRight = powdenest(root(parse_expr(ecuacion).rhs, fac ),
+    eqRight = powdenest(root(parse_expr(equation).rhs, fac ),
                                       force=True)# Se suma al lado derecho el factor
-    eq = Eq(eqLeft, eqRight) # Se combinan los dos lados en una nueva ecuación, en formato latex
+    eq = str(Eq(eqLeft, eqRight)) # Se combinan los dos lados en una nueva ecuación, en formato latex
     eqLatex = latex(Eq(eqLeft, eqRight)) # Se combinan los dos lados en una nueva ecuación, en formato latex
-    return {'sympy' : eq , 'latex' : eqLatex }  #Se devuelve la ecuacion en ambos formatos
+    return {'sympy' : eq , 'latex' : eqLatex }  #Se devuelve la equation en ambos formatos
 
-def Simplificar(ecuacion, factor):
-    eqLeft = powdenest(simplify(parse_expr(ecuacion).lhs, inverse=True), force=True)  # Se suma al lado izquierdo el factor
-    eqRight = powdenest(simplify(parse_expr(ecuacion).rhs, inverse=True), force=True)  # Se suma al lado derecho el factor
-    eq = Eq(eqLeft, eqRight) # Se combinan los dos lados en una nueva ecuación, en formato latex
+def Simplificar(equation, factor):
+    eqLeft = powdenest(simplify(parse_expr(equation).lhs, inverse=True), force=True)  # Se suma al lado izquierdo el factor
+    eqRight = powdenest(simplify(parse_expr(equation).rhs, inverse=True), force=True)  # Se suma al lado derecho el factor
+    eq = str(Eq(eqLeft, eqRight)) # Se combinan los dos lados en una nueva ecuación, en formato latex
     eqLatex = latex(Eq(eqLeft, eqRight)) # Se combinan los dos lados en una nueva ecuación, en formato latex
-    return {'sympy' : eq , 'latex' : eqLatex }  #Se devuelve la ecuacion en ambos formatos
+    return {'sympy' : eq , 'latex' : eqLatex }  #Se devuelve la equation en ambos formatos
 
-def Expandir(ecuacion, factor):
-    eqLeft = powdenest(expand(parse_expr(ecuacion).lhs, inverse=True), force=True)  # Se suma al lado izquierdo el factor
-    eqRight = powdenest(expand(parse_expr(ecuacion).rhs, inverse=True), force=True)  # Se suma al lado derecho el factor
+def Expandir(equation, factor):
+    eqLeft = powdenest(expand(parse_expr(equation).lhs, inverse=True), force=True)  # Se suma al lado izquierdo el factor
+    eqRight = powdenest(expand(parse_expr(equation).rhs, inverse=True), force=True)  # Se suma al lado derecho el factor
     eqLatex = latex(Eq(eqLeft, eqRight)) # Se combinan los dos lados en una nueva ecuación, en formato latex
-    return {'sympy' : eq , 'latex' : eqLatex }  #Se devuelve la ecuacion en ambos formatos
+    return {'sympy' : equation , 'latex' : eqLatex }  #Se devuelve la equation en ambos formatos
 
